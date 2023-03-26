cee0a231cc547c314af0e57be7db918f1fe2296e
Vercasson Victor <vercasson1605@gmail.com>
2023-03-26 14:12:30 +0200

    merge

--- analysis_gui/forms/__init__.py
+++ analysis_gui/forms/__init__.py
@@ -4,3 +4,4 @@ from .file_selection_form import FileSelectionForm
 from .histogram_form import HistogramUI
 from .reference_selection_form import ReferenceSelectionForm
 from .circle_form import CircleForm
+from .num_symptoms_form import NumSymptomsUI
---
+++ analysis_gui/forms/num_symptoms_form.py
@@ -0,0 +1,33 @@
+from .form import Form
+from .common import *
+from . import common
+from ipywidgets import ToggleButtons
+import numpy as np
+
+class NumSymptomsUI(Form):
+    
+    def __init__(self):
+        super().__init__(
+            output=Output(width="fit-content"),
+            layout=Layout(
+                width="max-content",
+                grid_gap="10px",
+                align_items="flex-start",
+                overflow="visible"
+            )
+        )
+        
+        
+    def init(self):
+        # Computing sum of symptoms in DataFrame
+        sympt_per_questionnaire = pd.DataFrame(np.zeros((common.df.shape[1]-5,3)), index = common.df.iloc[:,4:-1].columns, columns = ['Specific symptoms', 'Compound symptoms', 'Total'])
+        sympt_per_questionnaire['Specific symptoms'] = (common.df.iloc[:,4:-1]==1).sum(axis = 0)
+        sympt_per_questionnaire['Compound symptoms'] = (common.df.iloc[:,4:-1]==2).sum(axis = 0)
+        sympt_per_questionnaire['Total'] = (common.df.iloc[:,4:-1]>=1).sum(axis = 0)
+        sympt_per_questionnaire.to_excel("table1_symptomes_per_questionnaire.xlsx")
+        
+        self.children = [self._output]
+        
+        with self._output:
+            clear_output()
+            display(sympt_per_questionnaire)
